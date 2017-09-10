da0074f2eb836b66d15e219fcd8fbf6c50b0dff0
Desmond Campbell <Desmond.Campbell@glasgow.ac.uk>
2017-09-10 12:14:41 +0100

    First commit
    Testing package creation and git repository usage.

---
+++ R/square.R
@@ -0,0 +1,10 @@
+#' Square a number
+#'
+#' Takes a numeric and squares it
+#' @param x numeric to be squared
+#' @return squared value
+#' @export
+square <- function(x) x^2
+
+
+cube <- function(x) x^3
